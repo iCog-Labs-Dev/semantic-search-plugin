90b0f2321dedb65dfe02664172b45fbcbcaa985d
EyobAshenaki <ethioeyoba@gmail.com>
2023-08-16 14:15:58 +0300

    style(loader): Changed the default modal loader

--- webapp/src/Components/Modal/Loader/Loader.jsx
+++ webapp/src/Components/Modal/Loader/Loader.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import './Loader.css'
-function Loader({ loading }) {
-    if (!loading) return null
+import React from 'react';
+import './Loader.css';
+function Loader({loading}) {
+    if (!loading) {
+        return null;
+    }
     return (
-        <div class="loader"></div>
-    )
-
+        <div className='loader'>
+            <div/>
+            <div/>
+            <div/>
+        </div>
+    );
 }
 
-export default Loader
+export default Loader;
