294b147be6e8c98fa2aa17c64a4aace1e6456d16
surafel fikru <surafelfikru76@gmail.com>
2023-08-24 09:21:42 +0300

    Refactor UI to right side bar
    - Removed all components and started from blank
    - heavly modified index.tsx

--- webapp/src/Components/App.tsx
+++ webapp/src/Components/App.tsx
@@ -3,33 +3,14 @@ import { Store, Action } from 'redux';
 import { GlobalState } from '@mattermost/types/lib/store';
 
 import './App.css';
-import Modal from './Modal/Modal'
 declare global {
   interface Window { openSSModal: any; }
 }
 function App({ store }: { store: Store<GlobalState, Action<Record<string, unknown>>> }) {
-  const [openModal, setOpenModal] = useState(false)
-  
-  useEffect(() => {
-    return store.subscribe(() => {
-      const storeState: any = store.getState()
-      const state = storeState['plugins-com.sing.semantic-search']
-      setOpenModal(state.openModal)
-    })
-  }, [])
-  const changeOpenModal = (val: boolean) => {
-    const openModalAction = { type: { openModal: val } }
-    store.dispatch(openModalAction)
-  }
-  const handleModalBg = () => {
-    changeOpenModal(false)
-  }
+
   return (
-    <div className={`ss-container ${openModal ? '' : 'ss-hidden'}`}>
-      <div className='ss-container-bg' onClick={handleModalBg} />
-      <div className='ss-container-modal'>
-          <Modal/>
-      </div>
+    <div className="ss-root">
+      
     </div>
   )
 }
