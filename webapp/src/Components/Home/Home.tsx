99b674bbf52c4fa771e09dc123658e39c6c6d954
EyobAshenaki <ethioeyoba@gmail.com>
2023-09-05 13:51:56 +0300

    style(component): improve home component UI
    - Added static content
    - Styled home component to improve aesthetic

--- webapp/src/Components/Home/Home.tsx
+++ webapp/src/Components/Home/Home.tsx
@@ -1,14 +1,17 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 import './Home.css'
 
-
-function Home(store : any) {
+function Home() {
     return (
-        <div className='home-root'>
-            <h1>Search whatever you want!!!</h1>
+        <div className='home-container'>
+            <h2>Searching for past conversations made easy!</h2>
+            <h4> Simply enter a topic, and our plugin will search through previous messages, providing you with the answer and the messages that support it. Start searching smarter today!</h4>
+
+            {/* <h1>Need to find specific information in your chat history? </h1>
+            <p> Just enter a topic, and our app will search through previous messages to deliver the answer you're looking for. Get ready to save time and find what you need with ease.</p> */}
         </div>
-    )
+    );
 }
 
 export default Home;
