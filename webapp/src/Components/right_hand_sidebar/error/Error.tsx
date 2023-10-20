89c37f3e87ee25abdd07e31b5ecb56fd2f809876
EyobAshenaki <ethioeyoba@gmail.com>
2023-10-20 18:40:19 +0300

    chore(rhs): add server down error handling for search

--- webapp/src/Components/right_hand_sidebar/error/Error.tsx
+++ webapp/src/Components/right_hand_sidebar/error/Error.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './errorStyle.css'
 
 function Error({error} : any) {
+    // eslint-disable-next-line no-process-env
+    const apiURL = process.env.MM_PLUGIN_API_URL;
+    const [isOnline, setIsOnline] = useState<boolean>(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            let response;
+            try {
+                response = await fetch(`${apiURL}/img`, {
+                    method: 'HEAD',
+                });
+            } catch (err) {
+                // eslint-disable-next-line no-console
+                console.warn('Error:', (err as Error).message);
+            }
+
+            if (response?.ok) {
+                setIsOnline(true);
+            } else {
+                setIsOnline(false);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div className='ss-error-container'>
-            <p> { error.text } </p>
+            <p> { isOnline ? error.text : 'Server is down' } </p>
         </div>
     );
 }
