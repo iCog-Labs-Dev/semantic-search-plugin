b64524762959aa6329f4383129ace8b23bf95a2b
TollanBerhanu <tollanberhanu@gmail.com>
2023-11-07 13:15:48 +0300

    fix: update ping route fix: increase retry time

--- webapp/src/Components/admin_settings/is_engine_online_setting/IsEngineOnlineSetting.tsx
+++ webapp/src/Components/admin_settings/is_engine_online_setting/IsEngineOnlineSetting.tsx
@@ -3,7 +3,7 @@ import React, { Fragment, useEffect, useState } from 'react'
 import './isEngineOnlineSettingStyle.css'
 
 function IsEngineOnlineSetting() {
-    const RETRYTIMEINSECONDS = 10000;
+    const RETRYTIMEINSECONDS = 60 * 1000;   
     // eslint-disable-next-line no-process-env
     const apiURL = process.env.MM_PLUGIN_API_URL;
     const [isOnline, setIsOnline] = useState<boolean>(true);
@@ -13,7 +13,7 @@ function IsEngineOnlineSetting() {
         const interval = setInterval(async () => {
             let response;
             try {
-                response = await fetch(`${apiURL}/ping`, {
+                response = await fetch(`${apiURL}/root/ping`, {
                     method: 'HEAD',
                 });
             } catch (err) {
